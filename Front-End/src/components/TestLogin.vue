ad5a12788107cd3582234e51be42d9ccd3f93156
hafsa <hafsaarrab@gmail.com>
2023-12-23 16:05:08 +0100

    changes for npm run serve

--- Front-End/src/components/TestLogin.vue
+++ Front-End/src/components/TestLogin.vue
@@ -1,34 +1,34 @@
 <template>
-    <head>
-        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    </head>
-    <div class="body">
-        <div class="leftSide">
+    <div>
+        <head>
+          <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+        </head>
+        <div class="body">
+          <div class="leftSide">
             <div class="cercle1"></div>
             <div class="cercle2"></div>
-        </div>
-        <div class="rightSide">
+          </div>
+          <div class="rightSide">
             <div class="LoginTitre">
-                <p class="titre">Hello Again!</p>
-                <p class="text">Welcome Back</p>
-                <form>
-                    <div class="logoEmail">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <g opacity="0.3">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M1.5 5.25L2.25 4.5H21.75L22.5 5.25V18.75L21.75 19.5H2.25L1.5 18.75V5.25ZM3 6.8025V18H21V6.804L12.465 13.35H11.55L3 6.8025ZM19.545 6H4.455L12 11.8035L19.545 6Z"
-                                    fill="#333333" />
-                            </g>
-                        </svg>
-                    </div>
+              <p class="titre">Hello Again!</p>
+              <p class="text">Welcome Back</p>
+              <form>
+                <div class="logoEmail">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <g opacity="0.3">
+                      <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 5.25L2.25 4.5H21.75L22.5 5.25V18.75L21.75 19.5H2.25L1.5 18.75V5.25ZM3 6.8025V18H21V6.804L12.465 13.35H11.55L3 6.8025ZM19.545 6H4.455L12 11.8035L19.545 6Z" fill="#333333" />
+                    </g>
+                  </svg>
+                </div>
 
-                    <input type="text" id="email" placeholder="Email Address">
-                    <input type="password" id="password" placeholder="Password">
-                    <input type="button" id="buttonLogin" value="Login">
-                </form>
+                <input type="text" id="email" placeholder="Email Address">
+                <input type="password" id="password" placeholder="Password">
+                <input type="button" id="buttonLogin" value="Login">
+              </form>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
 </template>
 
 <style>
